e6986428d01e5d8d389c4595eec96864063afa17
Noel Cower <ncower@gmail.com>
2019-02-08 20:26:34 -0800

    Add ipxe generation endpoint & persistence
    The former allows you to generate an ipxe script and, if persistence
    auth is turned on, save the resulting config for later. When saved, the
    server will return a an ID for the ipxe script that can be passed to
    the /boot/:id path. Additional cfg_* arguments can be passed to the
    boot path to also configure the autoinstall.cfg.

--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.12
 require (
 	github.com/NYTimes/gziphandler v1.0.1
 	github.com/julienschmidt/httprouter v1.2.0
+	github.com/tv42/zbase32 v0.0.0-20160707012821-501572607d02
+	golang.org/x/crypto v0.0.0-20190208162236-193df9c0f06f
 	golang.org/x/sys v0.0.0-20190204203706-41f3e6584952
 )
--- main.go
+++ main.go
@@ -3,23 +3,33 @@ package main
 import (
 	"bytes"
 	"context"
+	"crypto/hmac"
+	"crypto/sha256"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
 	"path"
 	"runtime"
+	"sort"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	gziphandler "github.com/NYTimes/gziphandler"
 	"github.com/julienschmidt/httprouter"
+	"github.com/tv42/zbase32"
+	"golang.org/x/crypto/bcrypt"
 	"golang.org/x/sys/unix"
 )
+
+var authHash []byte
 
 var validators = map[string]Validator{
 	"disk":              Validators{Nonempty, IsAbsPath},
@@ -80,19 +90,38 @@ func main() {
 
 	log.SetFlags(log.LstdFlags | log.LUTC | log.Lshortfile | log.Lmicroseconds)
 
-	network := flag.String("N", "tcp", "listen `network` (tcp, unix)")
-	listen := flag.String("L", "127.0.0.1:8096", "listen `address`")
-	basePath := flag.String("p", "/autoinstall.cfg", "`path` to serve from")
-	logAccess := flag.Bool("a", false, "log access")
-	shutdownTimeout := flag.Duration("st", time.Second*10, "shutdown `timeout`")
+	var (
+		minPersistedConfigs = flag.Int("pm", 100, "`min`imum number of persisted configs to retain")
+		bootPass            = flag.String("p", "", "boot persistence `password` (may be a bcrypt hash)")
+		bootPassCost        = flag.Int("pc", 12, "boot persistence token `cost`")
+		network             = flag.String("N", "tcp", "listen `network` (tcp, unix)")
+		listen              = flag.String("L", "127.0.0.1:8096", "listen `address`")
+		logAccess           = flag.Bool("a", false, "log access")
+		shutdownTimeout     = flag.Duration("st", time.Second*10, "shutdown `timeout`")
+	)
 	flag.Parse()
 
-	// Check server path
-	switch basePath := *basePath; {
-	case strings.IndexByte(basePath, ':') != -1:
-		log.Fatalf("path (%q) may not contain colons (':')", basePath)
-	case !path.IsAbs(basePath):
-		log.Fatalf("path (%q) must be absolute", basePath)
+	if *bootPass == "" {
+		*bootPass = os.Getenv("SAVE_PASSWORD")
+	}
+
+	// Create persistence hash (if set)
+	if pass := []byte(*bootPass); len(pass) == 0 {
+	} else if _, err := bcrypt.Cost(pass); err == nil {
+		authHash = pass
+	} else if authHash, err = bcrypt.GenerateFromPassword(pass, *bootPassCost); err != nil {
+		log.Fatalf("Unable to hash password (len=%d cost=%d): %v", len(pass), *bootPassCost, err)
+	} else {
+		log.Printf("Persistence hash = %q (may be reused)", authHash)
+	}
+
+	if len(authHash) > 0 {
+		min := *minPersistedConfigs
+		go func() {
+			for range time.Tick(time.Minute) {
+				purgeOldConfigs(min)
+			}
+		}()
 	}
 
 	// Create listener
@@ -105,7 +134,7 @@ func main() {
 	log.Printf("listening on %v", listener.Addr())
 
 	// Create server
-	sv := createServer(*basePath, *logAccess)
+	sv := createServer(*logAccess)
 
 	// Shut down server
 	go func() {
@@ -130,9 +159,11 @@ func main() {
 	}
 }
 
-func createServer(basePath string, logAccess bool) *http.Server {
+func createServer(logAccess bool) *http.Server {
 	mux := httprouter.New()
-	mux.GET(basePath, serveConfig)
+	mux.GET("/autoinstall.cfg", serveConfig)
+	mux.GET("/boot", serveBoot)
+	mux.GET("/boot/:id", serveSavedBoot)
 
 	var handler http.Handler = gziphandler.GzipHandler(mux)
 
@@ -157,8 +188,6 @@ func waitForSignal(signals ...os.Signal) <-chan os.Signal {
 	return out
 }
 
-const prefix = "#!/bin/sh\n"
-
 var shQuoteReplacer = strings.NewReplacer(
 	"`", "\\`",
 	`"`, `\"`,
@@ -173,7 +202,7 @@ func serveConfig(w http.ResponseWriter, req *http.Request, _ httprouter.Params) 
 
 	const minBufferSize = 600
 	buf := bytes.NewBuffer(make([]byte, 0, minBufferSize))
-	buf.WriteString(prefix)
+	buf.WriteString("#!/bin/sh\n")
 
 	if user, password, ok := req.BasicAuth(); ok {
 		if user != "" {
@@ -209,4 +238,219 @@ func serveConfig(w http.ResponseWriter, req *http.Request, _ httprouter.Params) 
 	w.Header().Set("Content-Length", strconv.Itoa(buf.Len()))
 	w.WriteHeader(http.StatusOK)
 	buf.WriteTo(w)
+}
+
+func serveBoot(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/plain")
+
+	query := req.URL.Query()
+
+	const minBufferSize = 600
+	buf := bytes.NewBuffer(make([]byte, 0, minBufferSize))
+	buf.WriteString("#!ipxe\n")
+
+	base, err := url.Parse(query.Get("base"))
+	if err != nil {
+		http.Error(w, "cannot parse baseurl: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	base.Fragment = ""
+	base.RawQuery = ""
+
+	formatPath := func(s string) string {
+		return s
+	}
+	if *base != (url.URL{}) {
+		base.RawQuery = base.Query().Encode()
+		base.Path = path.Clean(base.Path)
+		if base.Path == "." {
+			base.Path = "/"
+		}
+
+		formatPath = func(s string) string {
+			return path.Join("${base-url}", s)
+		}
+		fmt.Fprintln(buf, `set base-url`, base)
+	}
+
+	configURL := generateConfigURL(req, query)
+	if configURL != nil {
+		fmt.Fprintln(buf, `set config-url`, configURL)
+	}
+
+	kernel := query.Get("kernel")
+	if kernel == "" {
+		kernel = "vmlinuz"
+	}
+	io.WriteString(buf, "kernel "+formatPath(kernel))
+
+	if passDefaults, _ := strconv.ParseBool(query.Get("defaults")); passDefaults {
+		// Default arguments
+		io.WriteString(buf, ` root=/dev/null ip=dhcp vconsole.keymap=us locale.LANG=en_US.UTF-8`)
+	}
+
+	for _, arg := range query["append"] {
+		io.WriteString(buf, " "+arg)
+	}
+
+	if configURL != nil {
+		io.WriteString(buf, ` auto autourl=${config-url}`)
+	}
+
+	io.WriteString(buf, "\n")
+
+	initrd := query.Get("initrd")
+	if initrd == "" && query["initrd"] != nil {
+		initrd = "initrd"
+	}
+	if initrd != "" {
+		io.WriteString(buf, "initrd "+formatPath(initrd)+"\n")
+	}
+
+	fmt.Fprintln(buf, "boot")
+
+	allowSave := isAuthed(req)
+	if save, _ := strconv.ParseBool(query.Get("save")); allowSave && save {
+		key := saveConfig(buf.Bytes())
+		buf.Reset()
+		buf.WriteString(key)
+	}
+
+	w.Header().Set("Content-Length", strconv.Itoa(buf.Len()))
+	w.WriteHeader(http.StatusOK)
+	buf.WriteTo(w)
+}
+
+type savedConfig struct {
+	ID     string
+	From   time.Time
+	Config string
+}
+
+func (s *savedConfig) String() string {
+	if s == nil {
+		return ""
+	}
+	return s.Config
+}
+
+func (s *savedConfig) Len() int {
+	if s == nil {
+		return 0
+	}
+	return len(s.Config)
+}
+
+// TODO: Replace with bolt, probably, so these can be persisted.
+// TODO: Also allow removal of existing configs before long-term persistence is added.
+var allSavedConfigs = map[string]*savedConfig{}
+var savedConfigsLock sync.RWMutex
+
+func purgeOldConfigs(keepMinNewest int) {
+	current := getSavedConfigs()
+	if len(current) < keepMinNewest {
+		return
+	}
+
+	sort.Slice(current, func(i, j int) bool {
+		return current[j].From.Before(current[j].From)
+	})
+
+	current = current[:len(current)-keepMinNewest]
+	if len(current) == 0 {
+		return
+	}
+
+	savedConfigsLock.Lock()
+	defer savedConfigsLock.Unlock()
+	for _, v := range current {
+		delete(allSavedConfigs, v.ID)
+	}
+}
+
+func getSavedConfigs() []*savedConfig {
+	savedConfigsLock.RLock()
+	defer savedConfigsLock.RUnlock()
+	values := make([]*savedConfig, 0, len(allSavedConfigs))
+	for _, v := range allSavedConfigs {
+		values = append(values, v)
+	}
+	return values
+}
+
+func saveConfig(config []byte) (key string) {
+	h := hmac.New(sha256.New, authHash)
+	h.Write(config)
+	sum := h.Sum(make([]byte, 0, h.Size()))
+	key = zbase32.EncodeToString(sum)
+
+	ref := &savedConfig{
+		ID:     key,
+		From:   time.Now(),
+		Config: string(config),
+	}
+
+	savedConfigsLock.Lock()
+	defer savedConfigsLock.Unlock()
+	allSavedConfigs[key] = ref
+
+	return key
+}
+
+func serveSavedBoot(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	key := params[0].Value
+
+	savedConfigsLock.RLock()
+	config := allSavedConfigs[key]
+	savedConfigsLock.RUnlock()
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Content-Length", strconv.Itoa(config.Len()))
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, config.String())
+}
+
+var configURLBase = url.URL{
+	Scheme: "https",
+	Host:   "void.spiff.io",
+	Path:   "/autoinstall.cfg",
+}
+
+func isAuthed(req *http.Request) bool {
+	password := req.Header.Get("Save-Token")
+	return len(authHash) > 0 && password != "" &&
+		bcrypt.CompareHashAndPassword(authHash, []byte(password)) == nil
+}
+
+func generateConfigURL(req *http.Request, query url.Values) *url.URL {
+	configParams := url.Values{}
+	for k, v := range query {
+		if strings.HasPrefix(k, "cfg_") && k != "cfg_" {
+			k = k[4:]
+			configParams[k] = v
+		}
+	}
+
+	user, password, haveAuth := req.BasicAuth()
+	if len(configParams) == 0 && !haveAuth {
+		return nil
+	}
+
+	configURL := configURLBase
+	configURL.RawQuery = configParams.Encode()
+	configURL.Fragment = ""
+	if haveAuth {
+		configURL.User = url.UserPassword(user, password)
+	}
+
+	// Add &/autoinstall.cfg to the end of the query string. This is needed to get around
+	// xbps-uhelper's fetch command not removing query strings from URLs, so when it fetches
+	// a file, it will include the query string in the basename. In particular, however, we do
+	// not want the / in this parameter encoded.
+	if configURL.RawQuery != "" {
+		configURL.RawQuery += "&/autoinstall.cfg"
+	}
+
+	return &configURL
 }
